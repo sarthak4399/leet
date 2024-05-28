aafa6d5394c2c0f2bc588a1c80a39aba7784da51
sarthak4399 <sarthakkhandare21@gmail.com>
2024-05-28 11:29:43 +0530

    feat: Add code to get equal substrings within budget

---
+++ Get Equal Substrings Within Budget.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+
+
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        
+    }
+};
+
+int main(){
+    Solution s ;
+    string str = "abcd";
+    string t = "bcdf";
+    int maxCost = 3;
+    cout << s.equalSubstring(str,t,maxCost) << endl;
+}

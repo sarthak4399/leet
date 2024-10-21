9ec976e6c8cf473c60a5375751483f4debda64ba
sarthak4399 <sarthakkhandare21@gmail.com>
2024-10-21 21:59:30 +0530

    Refactor C++ code to include a solution for splitting a string into the maximum number of unique substrings

---
+++ 1593. Split a String Into the Max Number of Unique Substrings.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<unordered_set>
+using namespace std ;
+
+
+class Solution {
+
+public:
+    int maxUniqueSplit(string s) {
+        int unique_count ;
+        unordered_set<string> unique_set;
+        unique_count = 0;
+        for (int i = 0 ; i < s.size(); i++){
+            string temp = "";
+            for (int j = i ; j < s.size(); j++){
+                temp += s[j];
+                if (unique_set.find(temp) == unique_set.end()){
+                    unique_set.insert(temp);
+                    unique_count++;
+                    i = j;
+                    break;
+                }
+            }
+        }
+
+
+        return unique_count;
+
+
+    }
+};
+
+
+int main () {
+    Solution s;
+    string str = "ababccc";
+    std::cout<<s.maxUniqueSplit(str)<<std::endl;
+    return 0;
+}

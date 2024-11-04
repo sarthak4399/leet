07506dc5d79a061995a0fd7a555afee64570ca56
sarthak4399 <sarthakkhandare21@gmail.com>
2024-11-04 19:00:30 +0530

    Refactor C++ solution for String Compression III by removing commented code and improving readability

--- 3163. String Compression III.cpp
+++ 3163. String Compression III.cpp
@@ -7,21 +7,15 @@ class Solution {
 public:
     string compressedString(string word) {
         if (word.empty()) return "";
-        
         string comp = "";
         int i = 0;
-
         while (i < word.length()) {
             char letter = word[i];
             int count = 0;
-
-            // Count up to 9 consecutive occurrences of letter
             while (i < word.length() && word[i] == letter && count < 9) {
                 count++;
                 i++;
             }
-
-            // Append count followed by the character to comp
             comp += to_string(count) + letter;
         }
 
@@ -33,8 +27,9 @@ int main() {
     Solution s;
     
     string word = "abcde";
-    cout << s.compressedString(word) << endl;  // Expected output: "1a1b1c1d1e"
-    
+    cout << s.compressedString(word) << endl; 
+
     word = "aaaaaaaaaaaaaabb";
-    cout << s.compressedString(word) << endl;  // Expected output: "9a5a2b"
-}
+    cout << s.compressedString(word) << endl;  
+    
+    }
